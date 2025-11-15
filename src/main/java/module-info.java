6652ec105c5e3f8238ec98fa3e4e3e9d5376e6c4
Pranav Gupta <kpranav.in@gmail.com>
2025-11-15 23:44:21 +0530

    Remove all files to clean repository

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example._20lpa_code_practice {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example._20lpa_code_practice to javafx.fxml;
-    exports com.example._20lpa_code_practice;
-}
